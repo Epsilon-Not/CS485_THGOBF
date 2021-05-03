de5021becc3b0dabac61caa9f166aa8a1d977aa5
Ck Will <chwilliams@epsilonsystems.com>
2021-05-02 19:51:48 -0700

    MainMenu

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : Player
+{
+    private void Update()
+    {
+        if(Input.GetButtonDown("Pause Key"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
+ 
